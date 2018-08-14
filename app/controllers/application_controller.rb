2cab090e600ddc609731a2fabe44535b4f5252b8
Felice Forby <forby.1@gmail.com>
2018-08-15 06:37:10 +0900

    application controller configuration and helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,26 @@
+require 'rack-flash'
+
 class ApplicationController < Sinatra::Base
 	
 	configure do
-		set :views, "app/views"
-		set :public_dir, "public"
+		set :views, 'app/views'
+		set :public_dir, 'public'
+		enable :sessions
+		set :session_secret, 'yasai'
+		use Rack::Flash, :sweep => true
 	end
 	
 	get '/' do
 		"Hello World!"
+	end
+	
+	helpers do
+		def current_user
+			@current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+		end
+		
+		def logged_in?
+			!!current_user
+		end
 	end
 end
