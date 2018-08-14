e44b15beee31c6bdd7dc91e579313354bc5af661
Felice Forby <forby.1@gmail.com>
2018-08-15 08:26:41 +0900

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
 	has_secure_password
 	has_many :farms
 	has_many :vegetables, through: :farms
+	
+	validates :username, :email, :password, presence: true
+	validates :email, uniqueness: true
 end
