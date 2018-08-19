7adfd4c09d6e5c19ddc783f691910bcd74111a86
Felice Forby <forby.1@gmail.com>
2018-08-20 07:30:44 +0900

    remove unnessary database code and yaml file after db fix

--- config/environment.rb
+++ config/environment.rb
@@ -4,11 +4,6 @@ ENV['RACK_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-# ActiveRecord::Base.establish_connection(
-# 	:adapter => "sqlite3",
-# 	:database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-# )
-
 ActiveRecord::Base.establish_connection(
   ENV['DATABASE_URL'] ||  { :adapter => "sqlite3",
                             :database => "db/#{ENV['SINATRA_ENV']}.sqlite" }
