9b31861d89f8ebeb2d6deaba366ee29777b5a527
Felice Forby <forby.1@gmail.com>
2018-08-19 12:47:39 +0900

    add farm spec

---
+++ spec/models/farm_spec.rb
@@ -0,0 +1,15 @@
+RSpec.describe 'Farm' do
+  before do
+    @user = User.create(username: "Joe Farmer", email: "joe@aol.com", password: "farmer")
+    @farm = Farm.create(name: "Hilltop Farm", location: "Ohio", user_id: @user.id)
+  end
+  
+  it "can make a slug from name" do
+    expect(@farm.slug).to eq("hilltop-farm")
+  end
+  
+  it "can find a farm with the slug" do
+    slug = @farm.slug
+    expect(Farm.find_by_slug(slug)).to eq(@farm)
+  end
+end
