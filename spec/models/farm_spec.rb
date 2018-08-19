35c579968ce17d803e9cfe6a0fc78245e81d00ee
Felice Forby <forby.1@gmail.com>
2018-08-19 12:50:23 +0900

    small edit to farm spec to include apostrophe in name

--- spec/models/farm_spec.rb
+++ spec/models/farm_spec.rb
@@ -1,11 +1,11 @@
 RSpec.describe 'Farm' do
   before do
     @user = User.create(username: "Joe Farmer", email: "joe@aol.com", password: "farmer")
-    @farm = Farm.create(name: "Hilltop Farm", location: "Ohio", user_id: @user.id)
+    @farm = Farm.create(name: "Joe's Farm", location: "Ohio", user_id: @user.id)
   end
   
   it "can make a slug from name" do
-    expect(@farm.slug).to eq("hilltop-farm")
+    expect(@farm.slug).to eq("joes-farm")
   end
   
   it "can find a farm with the slug" do
