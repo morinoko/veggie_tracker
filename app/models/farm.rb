52ba10b1dd1d28f2847a92917bb9fa8e59102f42
Felice Forby <forby.1@gmail.com>
2018-08-19 13:03:43 +0900

    add methods for slugigying name and finding by slug

--- app/models/farm.rb
+++ app/models/farm.rb
@@ -1,4 +1,12 @@
 class Farm < ActiveRecord::Base
 	belongs_to :user
 	has_many :vegetables
+	
+	def slug
+  	name.downcase.gsub(" ", "-").gsub("'", "")
+  end
+  
+  def self.find_by_slug(slug)
+    Farm.find { |farm| farm.slug == slug }
+  end
 end
