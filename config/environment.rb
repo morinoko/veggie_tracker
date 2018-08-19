4ed0a53e8043d7421845e46a3acd2dc935770652
Felice Forby <forby.1@gmail.com>
2018-08-19 15:24:15 +0900

    configure database for use with heroku

--- config/environment.rb
+++ config/environment.rb
@@ -4,9 +4,11 @@ ENV['RACK_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.establish_connection(
-	:adapter => "sqlite3",
-	:database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+# ActiveRecord::Base.establish_connection(
+# 	:adapter => "sqlite3",
+# 	:database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+# )
+
+set :database_file, 'database.yml'
 
 require_all 'app'
