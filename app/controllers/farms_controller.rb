9a5e417733a6cbd49a0b05c93f628001dd46a693
Felice Forby <forby.1@gmail.com>
2018-08-20 08:38:30 +0900

    add new farm page, route, and tests (WIP)

--- app/controllers/farms_controller.rb
+++ app/controllers/farms_controller.rb
@@ -1,5 +1,19 @@
 class FarmsController < ApplicationController
   
+  get '/farms/new' do
+    if logged_in?
+      erb :'farms/new'
+    else
+      flash[:notice] = "You need to be logged in to do that!"
+      
+      redirect to "/login"
+    end
+  end
+  
+  post '/farm' do
+    
+  end
+  
   get '/farms/:slug' do
     @farm = Farm.find_by_slug(params[:slug])
     
--- spec/controllers/farms_controller_spec.rb
+++ spec/controllers/farms_controller_spec.rb
@@ -18,4 +18,38 @@ RSpec.describe FarmsController, :type => :controller do
     end  
   end
   
+  describe "farm create page" do
+    before do
+      @user = User.create(username: "Ria", email: "ria@gmail.com", password: "meow")
+    end
+    
+    context "logged in" do
+      it "loads the new farm form" do
+        params = {
+          :username => "Ria",
+          :password => "meow"
+        }
+        
+        post '/login', params
+        
+        get '/farms/new'
+        
+        expect(last_response.status).to eq(200)
+        expect(last_response.body).to include("Create a new farm")
+      end
+    end
+    
+    context "logged out" do
+      it "redirects the user to login" do
+        get '/logout'
+        get '/farms/new'
+        
+        expect(last_response.redirect?).to be_truthy
+        follow_redirect!
+        expect(last_request.path).to eq('/login')
+        expect(last_response.body).to include("You need to be logged in to do that")
+      end
+    end
+  end
+  
 end
