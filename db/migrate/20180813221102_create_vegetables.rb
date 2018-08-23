eba556bd04ac27bf6cf0179e4537f271733be12a
Felice Forby <forby.1@gmail.com>
2018-08-24 07:38:31 +0900

    edit create vegetables migration

--- db/migrate/20180813221102_create_vegetables.rb
+++ db/migrate/20180813221102_create_vegetables.rb
@@ -2,8 +2,7 @@ class CreateVegetables < ActiveRecord::Migration[5.2]
   def change
 	  create_table :vegetables do |t|
 		  t.string :name
-		  t.text :annual_planting_start_date
-		  t.text :annual_planting_end_date
+		  t.text :planting_season
 		  t.integer :farm_id
 		end
   end
