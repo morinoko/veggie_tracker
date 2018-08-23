58dbacdd395ca1c03ece5d01c498d9013fbdecf0
Felice Forby <forby.1@gmail.com>
2018-08-24 08:35:10 +0900

    begin building new vegetable form

--- app/controllers/vegetables_controller.rb
+++ app/controllers/vegetables_controller.rb
@@ -1,3 +1,21 @@
+require 'date'
+
 class VegetablesController < ApplicationController
+  
+  get '/vegetables/new' do
+    if logged_in?
+      @user = current_user
+      
+      erb :'vegetables/new'
+    else
+     flash[:notice] = "You need to be logged in to do that!"
+     
+     redirect to '/login'
+    end
+  end
+  
+  post '/vegetables' do
+    
+  end
   
 end
