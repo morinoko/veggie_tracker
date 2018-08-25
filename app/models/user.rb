bb776d617d4e4b9cc434e24e2d4d90c08fb805ac
Felice Forby <forby.1@gmail.com>
2018-08-25 09:06:22 +0900

    make username unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 	has_many :vegetables, through: :farms
 	
 	validates :username, :email, :password, presence: true
-	validates :email, uniqueness: true
+	validates :email, :username, uniqueness: true
 	
 	def slug
   	self.username.downcase.gsub(" ", "-").gsub("'", "")
