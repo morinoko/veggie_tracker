62c012cd02f713e0a8edfe60cf0654ac47f59caf
Felice Forby <forby.1@gmail.com>
2018-08-25 16:03:01 +0900

    add methods to show planting months as numbers or names

--- app/models/vegetable.rb
+++ app/models/vegetable.rb
@@ -1,7 +1,13 @@
 class Vegetable < ActiveRecord::Base
 	has_and_belongs_to_many :farms
 	
-	def planting_months
+	def planting_months_as_numbers
   	self.planting_season.split(" ").collect { |month| month.to_i }
+  end
+  
+  def planting_months_as_names
+    Date::MONTHNAMES.select.with_index do |month, index|
+      month if self.planting_months_as_numbers.include?(index)
+    end
   end
 end
