f2123b269ec2dbacbe7271c4427ffd3e95d77aba
Felice Forby <forby.1@gmail.com>
2018-09-01 11:04:01 +0900

    i18n setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,28 @@
 require 'rack-flash'
 require 'date'
+require 'i18n'
+require 'i18n/backend/fallbacks'
 
 class ApplicationController < Sinatra::Base
 	
 	configure do
 		set :views, 'app/views'
 		set :public_dir, 'public'
+		
 		enable :sessions
 		set :session_secret, 'yasai'
+		
 		use Rack::Flash, :sweep => true
+		
+		I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
+    I18n.load_path = Dir[File.join(settings.root, 'locales', '*.yml')]
+    I18n.backend.load_translations
 	end
+	
+	before '/:locale/*' do
+    I18n.locale       =       params[:locale]
+    request.path_info = '/' + params[:splat ][0]
+  end
 	
 	get '/' do
   	if logged_in?
@@ -27,5 +40,11 @@ class ApplicationController < Sinatra::Base
 		def logged_in?
 			!!current_user
 		end
+		
+		def find_template(views, name, engine, &block)
+      I18n.fallbacks[I18n.locale].each { |locale|
+        super(views, "#{name}.#{locale}", engine, &block) }
+      super(views, name, engine, &block)
+    end
 	end
 end
