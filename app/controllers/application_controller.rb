34d16af00896e578a0600f380770f31c0037b9c1
Felice Forby <forby.1@gmail.com>
2018-09-09 15:22:32 +0900

    move helpers for months into separate module

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,10 @@ require 'rack-flash'
 require 'date'
 require 'i18n'
 require 'i18n/backend/fallbacks'
+require './sinatra/months'
 
 class ApplicationController < Sinatra::Base
+  helpers Sinatra::Months
 	
 	configure do
 		set :views, 'app/views'
@@ -55,47 +57,6 @@ class ApplicationController < Sinatra::Base
     
     def l(*args)
   		I18n.l(*args)
-    end
-		
-		def this_month
-  		month = Time.now.month
-  		I18n.t('date.month_names')[month]
-    end
-    
-    def month_name(month)
-      I18n.t('date.month_names')[month]
-    end
-    
-    def next_month_number(month)
-      if month == 12
-        1
-      else
-        month + 1
-      end
-    end
-    
-    def previous_month_number(month)
-      if month == 1
-        12
-      else
-        month - 1
-      end
-    end
-    
-    def next_month_name(month)
-      if month == 12
-        I18n.t('date.month_names')[1]
-      else
-        I18n.t('date.month_names')[month + 1]
-      end
-    end
-    
-    def previous_month_name(month)
-      if month == 1
-        I18n.t('date.month_names')[12]
-      else
-        I18n.t('date.month_names')[month - 1]
-      end
-    end
+    end		
 	end
 end
---
+++ sinatra/months.rb
@@ -0,0 +1,48 @@
+require 'sinatra/base'
+
+module Sinatra
+  module Months
+    def this_month
+  		month = Time.now.month
+  		I18n.t('date.month_names')[month]
+    end
+    
+    def month_name(month)
+      I18n.t('date.month_names')[month]
+    end
+    
+    def next_month_number(month)
+      if month == 12
+        1
+      else
+        month + 1
+      end
+    end
+    
+    def previous_month_number(month)
+      if month == 1
+        12
+      else
+        month - 1
+      end
+    end
+    
+    def next_month_name(month)
+      if month == 12
+        I18n.t('date.month_names')[1]
+      else
+        I18n.t('date.month_names')[month + 1]
+      end
+    end
+    
+    def previous_month_name(month)
+      if month == 1
+        I18n.t('date.month_names')[12]
+      else
+        I18n.t('date.month_names')[month - 1]
+      end
+    end
+  end
+  
+  helpers Months
+end
